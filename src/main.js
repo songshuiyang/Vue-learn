64e83530a116e7b8bcfab0afc4f2a7821300f577
songshuiyang <1459074711@qq.com>
2017-10-31 23:24:03 +0800

    注释 import Es6的语法

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
+import Vue from 'vue' // 等价 var Vue = require('./vue')
 import App from './App'
 import router from './router'
 
