aa74874f90b94e4692067ed386128fa7d072e6e4
songshuiyang <1459074711@qq.com>
2017-11-01 22:17:51 +0800

    在HTML5中，新加入了一个localStorage特性，这个特性主要是用来作为本地存储来使用的

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2 v-on:click="sayHello()">Vue Js</h2>
+    <input v-model="msg"/>
+
+    <h2 v-on:click="setStore(msg)">设置LocalStorage</h2>
+    <h2 v-on:click="getStore()">获得LocalStorage</h2>
     <!-- <ul>
       <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
       <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
@@ -9,19 +11,31 @@
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul> -->
   </div>
-</template>
+</template> 
 
 <script>
+import Store from '../store/store.js'
+
+console.log(Store)  
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: ''
+      msg: '',
+      item: []
     }
   },
   methods: {
     sayHello : function () {
       alert("11")
+    },
+    setStore : function (item) {
+      Store.save(item)
+    },
+    getStore : function () {
+      alert(Store.fetch())
+
     }
   }
 }
